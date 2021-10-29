33076f369278d2accadc8583d5b0558182df6d25
Lukas Kiefer <kiefermaniac@gmail.com>
2021-10-29 16:08:26 +0200

    read config from file, auto-disable on wrong config

--- send_telegram.py
+++ send_telegram.py
@@ -1,18 +1,35 @@
 import requests
 import os
 from logger import logger
+from load_config import *
+
+config = load_config('config.yml')
+bot_token = config['TELEGRAM']['BOT_TOKEN']
+bot_chatID = str(config['TELEGRAM']['BOT_CHAT_ID'])
+
+send_success = True
 
 def send_telegram(message):
 
-   bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
-   bot_chatID = os.getenv('TELEGRAM_BOT_CHAT_ID')
+   global send_success
 
-   for var in [bot_token, bot_chatID]:
-      if var is None:
-         return
+   if send_success:
 
-   send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + message
-   logger.info('TELEGRAM sending '+message)
-   
-   response = requests.get(send_text).json()
-   logger.debug('TELEGRAM '+str(response))
+      for var in [bot_token, bot_chatID]:
+         if var is None:
+            return
+
+      send_text = 'https://api.telegram.org/botX' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + message
+      logger.info('TELEGRAM sending '+message)
+      
+      response = requests.get(send_text)
+      logger.debug('TELEGRAM '+str(response.json()))
+
+      if response.status_code != 200:
+         logger.info('failed to send telegram message, disabling')
+         send_success = False
+      else:
+         send_success = True
+
+   else:
+      logger.debug('Not sending to telegram, last send failed')
